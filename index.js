f6b6b741ffc0156eb48a3364f25831a7bbf8fca8
Mridul5300 <mridulsarkar1157@gmail.com>
2025-07-31 04:13:17 +0600

    cors configure

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const dotenv = require('dotenv');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -10,7 +10,31 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
+const cors = require('cors'); 
+
+
+
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'http://localhost:5000', 
+  'https://subtle-semifreddo-9a6bed.netlify.app/'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  credentials: true,
+  optionsSuccessStatus: 204
+}));
+
+
 app.use(express.json());
 
 // DB connection
